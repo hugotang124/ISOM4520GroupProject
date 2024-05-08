6629215f9037444dec88063db64e49789b72b8af
Hugo Tang <hugotang124@gmail.com>
2024-05-08 16:51:40 +0800

    Finish optimization

--- PCAPtfStatArb.py
+++ PCAPtfStatArb.py
@@ -25,6 +25,7 @@ import tqdm
 
 
 SPY_SECTOR_ETF = ["XLC", "XLY", "XLP", "XLE", "XLF", "XLV", "XLI", "XLB", "XLRE", "XLK", "XLU"]
+BACKTEST_TABLE_FIELDS = ["Test Name", "Portfolio Return (%)", "Average Return (%)", "Volatility (%)", "VaR (%)", "MDD(%)","Alpha (%)", "Beta", "Sharpe Ratio"]
 
 class PCAPtfStatArb:
     
@@ -61,8 +62,11 @@ class PCAPtfStatArb:
             holder[ticker] = df
         
         self.benchmark_data = holder.pop(self.benchmark_sym)
-        self.benchmark_returns = pd.DataFrame(self.benchmark_data.Close.pct_change().dropna(), columns = ['returns'])
         self.close_df, self.open_df, self.return_df, self.std_returns_df = self.generalize_data(holder)
+
+        self.benchmark_data = self.benchmark_data.loc[self.close_df.index]
+        self.benchmark_data['returns'] = self.benchmark_data.Close.pct_change()
+        self.benchmark_returns = self.benchmark_data['returns'].loc[self.return_df.index]
         self.calculate_beta()
 
         
@@ -72,18 +76,16 @@ class PCAPtfStatArb:
         close_df.columns = list(holder.keys())
         open_df = pd.concat(list(map(lambda x: x.Open, holder.values())), axis = 1)
         open_df.columns = list(holder.keys())
-        return_df = close_df.pct_change(axis = 0).dropna()
+        return_df = close_df.pct_change(axis = 0)
         std_returns_df = (return_df - return_df.rolling(252, axis = 0).mean())/ return_df.rolling(252, axis = 0).std()
-        std_returns_df.dropna(inplace = True)
         return close_df, open_df, return_df, std_returns_df
     
     def calculate_beta(self):
-        self.beta_df = pd.DataFrame()     
-        for stock in self.return_df.columns:
-            self.beta_df[stock] = self.return_df[stock].rolling(22).cov(self.benchmark_returns) / self.return_df[stock].rolling(22).var()
-        
+        self.beta_df = pd.DataFrame() 
+        self.beta_df = self.return_df.rolling(22, axis = 0).cov(self.benchmark_returns) / self.return_df.rolling(22, axis = 0).var()
         # Beta calculation based on the previous close when opening the position
         self.beta_df.shift(1)
+
 
     def get_residuals(self, data, N = 1):
         pca = PCA(n_components = N)
@@ -119,28 +121,28 @@ class PCAPtfStatArb:
     
         cash_value = self.initial_cash
         for i in range(self.close_df.shape[0] + 1):
-            if i < min_data_points:
-                continue
-    
-            if i != self.close_df.shape[0] and self.close_df.index[i] < start_date:
+            if i != self.close_df.shape[0] and self.close_df.index[i] < self.start_date:
                 continue
     
             # Get Mark-to-Market Value of different positions after previous close
-            pnl = 0
+            daily_pnl = 0
             for stock, position_info in entry.items():
                 price, size = position_info.values()
                 if stock != "hedge":
-                    pnl += (price - self.close_df.iloc[i - 1, stock]) * size
+                    trade_pnl = (price - self.close_df.iloc[i - 1, stock]) * size
                 else:
-                    pnl += (price - self.benchmark_data.Close[i - 1]) * size
-                pnl -= commission_fee * abs(size)
-    
-            cash_value += pnl
-            pnls.append(pnl)
+                    trade_pnl = (price - self.benchmark_data.Close[i - 1]) * size
+                
+                trade_pnl -= commission_fee * abs(size)
+                position_info["pnl"] = trade_pnl
+                daily_pnl += trade_pnl
+                
+            cash_value += daily_pnl
+            pnls.append(daily_pnl)
     
             orders[self.close_df.index[i - 1]] = entry
     
-            if i >= self.close_df.shape[0] or self.close_df.index[i] > end_date:
+            if i >= self.close_df.shape[0] or self.close_df.index[i] > self.end_date:
                 break
     
             section_data = self.close_df.iloc[i - min_data_points:i, :]
@@ -148,7 +150,7 @@ class PCAPtfStatArb:
                 continue
     
             # Getting residuals and z-scores based on previous close
-            residuals = self.get_residuals(section_data, 2)
+            residuals = self.get_residuals(section_data, 1)
             residuals = np.stack(residuals, axis = 0)
             z_scores = self.z_score(residuals)
             zs = dict(enumerate(z_scores[-1, :], 1))
@@ -196,6 +198,10 @@ class PCAPtfStatArb:
         df['benchmark_val'] = df.benchmark_close / df.benchmark_close[0] * self.initial_cash
         df['ptf_return'] = df.ptf_value.pct_change()
         df['benchmark_return'] = df.benchmark_val.pct_change()
+
+        window = 252
+        roll_max = df.ptf_value.rolling(window, min_periods = 1).max()
+        df['max_drawdown'] = df.ptf_value / roll_max - 1
         
     
         return cash_value, df, orders        
@@ -214,37 +220,45 @@ class PCAPtfStatArb:
         df.to_csv(simulation_csv, index = True)
 
     
-    def analyze_results(self, df, orders):
+    def plot_graphs(self, df, orders):
         
         graph_storage = "Graphs"
         if not os.path.exists(graph_storage):
             os.mkdir(graph_storage)
 
         # Equity Curve
-        df['benchmark_close'] = self.benchmark_data.Close[(self.benchmark_data.index >= self.start_date - BDay(1)) & (self.benchmark_data.index <= self.end_date)]
-        df['benchmark_val'] = df.benchmark_close / df.benchmark_close[0] * self.initial_cash
-        df['ptf_return'] = df.ptf_value.pct_change()
-        df['benchmark_return'] = df.benchmark_val.pct_change()
+        equity_curve = plt.figure(figsize = (8, 8))
         plt.plot(df.ptf_value, label = "Strategy")
         plt.plot(df.benchmark_val, label = "Benchmark")
         plt.legend()
-        plt.savefig(os.path.join(graph_storage, "EquityCurveVSBenchmark.png"))
-
-        
-        df['ptf_return'] = df.ptf_value.pct_change().fillna(0)
+        equity_curve.savefig(os.path.join(graph_storage, "EquityCurveVSBenchmark.png"))
 
 
-        # Graph of Portfolio Beta
+
+        # Portfolio Beta
+        beta_curve = plt.figure(figsize = (8, 8))
         plt.plot(df.ptf_beta)
         plt.title('Portfolio Beta')
         plt.xlabel("Time")
         plt.ylabel("Beta")
         plt.savefig(os.path.join(graph_storage, "PortfolioBetaGraph.png"))
 
+        # Max Drawdown Curve
+        mdd_curve = plt.figure(figsize = (8, 8))
+        plt.plot(df.max_drawdown)
+        plt.title('Portfolio Max Drawdown')
+        plt.xlabel("Time")
+        plt.ylabel("Beta")
+        plt.savefig(os.path.join(graph_storage, "PortfolioMDDGraph.png"))
+
+
+
     def backtest_statistics(self, df):
 
-        ptf_returns = df.ptf_return.dropna()
-        ptf_return = (df.ptf_value[-1] - df.ptf_value[0]) / df.ptf_value[0]
+        df_b = df.copy().dropna()
+
+        ptf_returns = df_b.ptf_return
+        ptf_return = (df_b.ptf_value[-1] - df_b.ptf_value[0]) / df_b.ptf_value[0]
         avg_return = ptf_returns.mean()
         std_daily_return = ptf_returns.std()
         volatility = std_daily_return * np.sqrt(252)
@@ -253,16 +267,13 @@ class PCAPtfStatArb:
         z_score_var = norm.ppf(confidence_level)
         VaR_percentage = - z_score_var * std_daily_return
 
-        benchmark_returns = df.benchmark_return.dropna()
+        benchmark_returns = df_b.benchmark_return
         model = sm.OLS(ptf_returns, sm.add_constant(benchmark_returns)).fit()
         alpha, beta = model.params
 
         sharpe = avg_return / std_daily_return * np.sqrt(252)
 
-        window = 252
-        roll_max = df.ptf_value.rolling(window, min_periods = 1).max()
-        df['max_drawdown'] = df.ptf_value / roll_max - 1
-        max_drawdown = -df.max_drawdown.min()
+        max_drawdown = -df_b.max_drawdown.min()
 
         statistics = {
             "ptf_return": ptf_return,
@@ -276,25 +287,25 @@ class PCAPtfStatArb:
         }
         return df, statistics
     
-    def create_table(self):
+    def create_table(self, fields):
         # Display in table
         table = PrettyTable()
-        table.field_names = ["Test Name", "Portfolio Return (%)", "Average Return (%)", "Volatility (%)", "VaR (%)", "MDD(%)","Alpha (%)", "Beta", "Sharpe Ratio"]
+        table.field_names = fields
         return table
     
-    def add_data(self, table, data, test_name = "Backtest"):
-        # Multiply all values except sharpe and alpha
-        data = np.array(data)
-        data[:-2] *= 100
-        print(data)
-        table.add_row([test_name] + list(data))
-        return table
     
-    def display_table(self, table):
+    def run_simulation(self, max_pos, min_datapoints, beta_hedge = False):
+        end_cash_value, df, orders = self.simulation(max_pos, min_datapoints, beta_hedge)
+        self.plot_graphs(df, orders)
+        df, statistics = self.backtest_statistics(df)
+        table = self.create_table(BACKTEST_TABLE_FIELDS)
+        statistics = np.array(list(statistics.values()))
+        statistics[:-2] *= 100
+        statistics = np.round(statistics, 3)
+        table.add_row(["Backtest"] + list(statistics))
         print(table)
-    
-    def run_simulation(self, max_pos, min_pos):
-        pass
+
+        self.plot_graphs(df, orders)
 
     def cartesian(self, arrays, out = None):
         arrays = [np.asarray(x) for x in arrays]
@@ -314,7 +325,7 @@ class PCAPtfStatArb:
         return out
     
     # Grid Search Optimization
-    def optimize(self, start_date, end_date):
+    def optimize(self):
         max_pos_poss = list(range(1, 6))
         min_points_poss = list(range(10, 252, 10))
         all_possibilities = self.cartesian([max_pos_poss, min_points_poss])
@@ -328,13 +339,20 @@ class PCAPtfStatArb:
         
         optim_res = pd.concat(test_results)
         optim_res[["max_pos", "min_points"]] = all_possibilities
+        optim_res['backtest_name'] = "Case_" + optim_res["max_pos"] + optim_res["min_points"]
+        optim_res.sort_values(by = "ptf_return", inplace = True)
+        return optim_res
+    
+    def run_optimize(self):
+        pass
+        
 
 
 if __name__ == "__main__":
-    start_date = datetime(2019, 1, 1)
+    start_date = datetime(2022, 1, 1)
     end_date = datetime(2024, 1, 1)
     algo = PCAPtfStatArb(SPY_SECTOR_ETF, start_date, end_date)
 
     max_position = 3
     min_data_points = 170
-    algo.run_simulation(max_position, min_data_points)
+    algo.run_simulation(max_position, min_data_points, True)
